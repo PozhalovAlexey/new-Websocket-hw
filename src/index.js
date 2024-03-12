a22d9f636b145d2d0d69b9e94043313187cc9078
Alexey Pozhalov <pozhalov7@mail.ru>
2024-03-12 21:50:48 +0300

    Добавил обработку формы, и поступление данных от вебсокетов.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/style.css'
+
+import './js/widget';
+import './js/app';
+
+console.log(123123123)
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import Widget from './widget.js';
+
+const widget = new Widget('ws://localhost:3000');
+console.log(22222)
+widget.createForm();
+
+
+console.log(widget)
